6d2b2929fbf08cfc6233e322687f861567f00f4f
ed.xyz <91804938+EDED2314@users.noreply.github.com>
2022-10-15 17:01:28 -0400

    grade points bruh string

--- scripts/data.py
+++ scripts/data.py
@@ -399,7 +399,7 @@ class DataExtractor(BeautifulSoup):
                             .strip()
                         )
                     except IndexError:
-                        grade_points = 0.0
+                        grade_points = "0.0"
                         continue
 
                 except AttributeError:
